6ae94fd89d0cfa37b6d62d7e17c4217955650647
Willie Vogel <wivo1351@colorado.edu>
2021-04-20 16:23:05 -0600

    added second line of melody

--- src/index.js
+++ src/index.js
@@ -13,17 +13,22 @@ import * as Tone from 'tone';
 // wait one second before triggering the release
 //synth.triggerRelease(now + 1)
 
-// const synth = new Tone.Synth().toDestination();
-// const now = Tone.now()
-// synth.triggerAttackRelease("A3", "4n", now)
-// synth.triggerAttackRelease("C#4", "8n", now + 0.5)
-// synth.triggerAttackRelease("E4", "4n", now + 1)
-// synth.triggerAttackRelease("G#4", "8n", now + 1.5)
-
-const pingPong = new Tone.PingPongDelay("8n", 0.2).toDestination();
+const synth = new Tone.Synth().toDestination();
 const now = Tone.now()
-const synth = new Tone.Synth().connect(pingPong);
-synth.triggerAttackRelease("A3", "4n", now)
-synth.triggerAttackRelease("C#4", "8n", now + 0.5)
-synth.triggerAttackRelease("E4", "4n", now + 1)
-synth.triggerAttackRelease("G#4", "8n", now + 1.5)
+synth.triggerAttackRelease("C3", "4n", now)
+synth.triggerAttackRelease("E3", "8n", now + 0.5)
+synth.triggerAttackRelease("G3", "4n", now + 1)
+synth.triggerAttackRelease("B3", "8n", now + 1.5)
+
+synth.triggerAttackRelease("G3", "4n", now + 2.5)
+synth.triggerAttackRelease("E3", "8n", now + 3)
+synth.triggerAttackRelease("C3", "4n", now + 3.5)
+synth.triggerAttackRelease("B2", "8n", now + 4)
+
+// const pingPong = new Tone.PingPongDelay("8n", 0.4).toDestination();
+// const now = Tone.now()
+// const synth = new Tone.Synth().connect(pingPong);
+// synth.triggerAttackRelease("C3", "4n", now)
+// synth.triggerAttackRelease("F4", "8n", now + 0.5)
+// synth.triggerAttackRelease("G4", "4n", now + 1)
+// synth.triggerAttackRelease("B4", "8n", now + 1.5)
